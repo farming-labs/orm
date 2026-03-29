ee8824bf11d30a23306ebeb29d73d39594eb1481
Kinfe123 <kinfishtech@gmail.com>
2026-03-29 19:34:11 +0300

    fix: recursive bump

--- bump.config.ts
+++ bump.config.ts
@@ -4,16 +4,6 @@ export default defineConfig({
   commit: "chore: release v%s",
   tag: "v%s",
   push: true,
-  files: [
-    "packages/orm/package.json",
-    "packages/cli/package.json",
-    "packages/sql/package.json",
-    "packages/drizzle/package.json",
-    "packages/kysely/package.json",
-    "packages/mongo/package.json",
-    "packages/mongoose/package.json",
-    "packages/prisma/package.json",
-    "packages/runtime/package.json",
-  ],
+  recursive: true,
   execute: "pnpm build",
 });
