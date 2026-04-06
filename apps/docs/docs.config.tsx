c215a74741547fe25ef6df839932c867e13416d4
Kinfe123 <kinfishtech@gmail.com>
2026-04-07 01:40:39 +0300

    chore: neo4j icon

--- apps/docs/docs.config.tsx
+++ apps/docs/docs.config.tsx
@@ -138,6 +138,16 @@ const surrealdbIcon = icon(
   </svg>,
 );
 
+const neo4jIcon = icon(
+  <svg aria-hidden="true" viewBox="10 120 100 110" role="img">
+    <title>Neo4j</title>
+    <path
+      fill="currentColor"
+      d="M24.6 125.8c-3 1.5-5.8 4-7.8 7.3-2 3.3-2.8 6.8-2.5 10.3.3 6.3 3.8 12.1 9.5 15.3 5.3 3 11.3 2.3 17 1 7-1.8 13-2.5 19.3 1.3 0 0 0 0 .3 0 10.8 6.3 10.8 22.1 0 28.4 0 0 0 0-.3 0-6.3 3.8-12.3 3-19.3 1.3-5.5-1.5-11.5-2.3-17 1-5.8 3.3-9 9.3-9.5 15.3-.3 3.5.5 7 2.5 10.3 2 3.3 4.5 5.8 7.8 7.3 5.5 2.8 12.3 2.8 18-.5 5.3-3 7.8-8.8 9.3-14.3 2-7 4.3-12.6 10.8-16.1 6.3-3.8 12.3-3 19.3-1.3 5.5 1.5 11.5 2.3 17-1 5.8-3.3 9-9.3 9.5-15.3 0-.5 0-.8 0-1.3 0-.5 0-.8 0-1.3-.3-6.3-3.8-12.1-9.5-15.3-5.3-3-11.3-2.3-17-1-7 1.8-13 2.5-19.3-1.3-6.3-3.8-8.8-9.1-10.8-16.1-1.5-5.5-4-11.1-9.3-14.3-5.8-3.3-12.5-3.3-18 0z"
+    />
+  </svg>,
+);
+
 export default defineDocs({
   entry: "docs",
   theme: pixelBorder({
@@ -298,6 +308,7 @@ export default defineDocs({
     database: icon(<Database strokeWidth={1.5} />),
     braces: icon(<Braces strokeWidth={1.5} />),
     network: icon(<Network strokeWidth={1.5} />),
+    neo4j: neo4jIcon,
     server: icon(<Server strokeWidth={1.5} />),
     boxes: icon(<Boxes strokeWidth={1.5} />),
     code: icon(<Code2 strokeWidth={1.5} />),
