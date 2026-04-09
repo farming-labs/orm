ad60258f0ceb245c1491352b69d40c49c90a1f85
Kinfe123 <kinfishtech@gmail.com>
2026-04-09 04:55:20 +0300

    chore: update

--- apps/docs/next.config.ts
+++ apps/docs/next.config.ts
@@ -4,7 +4,11 @@ import { withDocs } from "@farming-labs/next/config";
 const repoRoot = path.resolve(process.cwd(), "..");
 
 export default withDocs({
+  outputFileTracingRoot: path.join(import.meta.dirname, "../.."),
   turbopack: {
     root: repoRoot,
+    resolveAlias: {
+      "@/docs.config": "./docs.config.tsx",
+    },
   },
 });
